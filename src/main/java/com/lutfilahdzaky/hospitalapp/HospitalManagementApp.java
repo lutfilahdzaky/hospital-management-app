d5c71417c252ead1aadc9ea1f83ae0cc5c7c00b7
Lutfilah Dzaky <28544046+lutfilahdzaky@users.noreply.github.com>
2025-01-09 01:21:07 +0700

    Initial commit

---
+++ src/main/java/com/lutfilahdzaky/hospitalapp/HospitalManagementApp.java
@@ -0,0 +1,12 @@
+package com.lutfilahdzaky.hospitalapp;
+
+/**
+ *
+ * @author Lutfilah Dzaky
+ */
+public class HospitalManagementApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
